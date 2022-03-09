ad6b8f75ecce914ed13fd669990c022fee4b8e29
alexgrent <51237839+alexgrent@users.noreply.github.com>
2022-03-09 08:14:23 +0100

    Observer implementation

--- src/FileWriting/IoTest.java
+++ src/FileWriting/IoTest.java
@@ -1,2 +1,77 @@
-package FileWriting;public class IoTest {
+package FileWriting;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.spec.ECField;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class IoTest {
+
+    public static void readContentOfFile(String filename) throws Exception{
+        int i;
+        FileInputStream fin = null;
+        fin = new FileInputStream(filename);
+        do{
+            i = fin.read();
+            if(i != -1) {
+                System.out.println((char) i);
+            }
+            }while (i != -1);
+
+            if(fin != null){
+                fin.close();
+            }
+    }
+
+    public static void readContentOfFileV2(String filename)throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+        String s;
+        while ((s=bufferedReader.readLine()) != null){
+            System.out.println(s);
+        }
+        bufferedReader.close();
+    }
+
+    public static void readContentOfFileV3(String filename)throws Exception{
+        FileReader fileReader = new FileReader(filename);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String s;
+        while ((s= bufferedReader.readLine()) != null){
+            System.out.println(s);
+        }
+        fileReader.close();
+    }
+
+    public static void readContentOfFileV4(String filename)throws Exception{
+        Scanner scanner = new Scanner(new File(filename));
+        while (scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+    }
+
+    public static void readContentOfFileV5(String filename) throws Exception
+    {
+        Stream<String> stream = Files.lines(Paths.get(filename));
+        stream.forEach(System.out::println);
+        stream.close();
+    }
+
+    public static void writeToFileV1(String filename)throws Exception{
+        FileOutputStream fout = null;
+        fout = new FileOutputStream( filename );
+        String toWrite = "HELLO";
+        fout.write( toWrite.getBytes() );
+        fout.close();
+    }
+
+    public static void writeToFileV2(String filename) throws Exception {
+        FileWriter writer = new FileWriter(filename);
+        String toWrite = "Hallo";
+        writer.write( toWrite );
+        writer.close();
+    }
+
 }
--- src/collections/Stack.java
+++ src/collections/Stack.java
@@ -1,2 +1,22 @@
-package collections;public class Stack {
+package collections;
+
+
+public class Stack {
+    public int stck[] = new int[10];
+    private int tos = -1;
+
+    public void push(int item){
+        stck[++tos] = item;
+    }
+
+    public int pop(){
+        if(tos<0){
+            System.out.printf("underflow");
+            return 0;
+        }
+        else {
+            return stck[tos--];
+        }
+    }
+
 }
